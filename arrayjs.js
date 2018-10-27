bb05684cd5f830c37087fcc63a6cabe835a6d428
Alberto Garcia <albertoinformatica97@gmail.com>
2018-10-27 23:33:19 +0200

    ya no se permiten dos arrays con el mismo nombre

--- arrayjs.js
+++ arrayjs.js
@@ -4,6 +4,13 @@ var Arrays = {
     addArray: function(nom, arr) {
         this.noms.push(nom);
         this.arrs.push(arr);
+    },
+    findArrByNom: function(nom) {
+        var posArr = this.noms.indexOf(nom);
+        return this.arrs[posArr];
+    },
+    nomsAllArrs: function() {
+        return this.noms;
     }
 }
 
@@ -14,17 +21,31 @@ function addArr() {
 
     if (elements.length === 0 || nomArr.length === 0) alert('Introduzca un nombre y uno o más elementos para añadir un array');
     else {
-        nomArr = nomArr.trim();
-        elements = elements.split(',');
-        elements = elements.map(e => e.trim());
-        Arrays.addArray(nomArr, elements);
+        nomArr = nomArr.trim();      
+        var nomsArrs = Arrays.nomsAllArrs();
 
-        var opt = document.createElement('option');
-        opt.value = nomArr;
-        opt.text = nomArr;
-        list.add(opt);
-
-        document.getElementById('nomArr').value = '';
-        document.getElementById('elementsArr').value = '';
+        if (nomsArrs.indexOf(nomArr) >= 0) {
+            alert('No se permiten dos Arrays con el mismo nombre');
+            document.getElementById('nomArr').value = '';
+        }
+        else {
+            elements = elements.split(',');
+            elements = elements.map(e => e.trim());
+            Arrays.addArray(nomArr, elements);
+    
+            var opt = document.createElement('option');
+            opt.value = nomArr;
+            opt.text = nomArr;
+            list.add(opt);
+    
+            document.getElementById('nomArr').value = '';
+            document.getElementById('elementsArr').value = '';
+        }
     }
+}
+
+function changeListArr() {
+    var nom = document.getElementById('listArrs').value;
+    console.log(nom);
+    document.getElementById('selectedArray').value = Arrays.findArrByNom(nom);
 }
